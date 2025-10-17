f071582c369c09aed7d88147de716205fdd673b2
AryaSaravanan <118869865+AryaSaravanan@users.noreply.github.com>
2025-10-17 15:03:57 -0400

    Implemented the getSubBreeds method in the DogApiBreedFetcher class

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -6,7 +6,10 @@ import okhttp3.Response;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.*;
 
 /**
@@ -23,13 +26,31 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @return list of sub breeds for the given breed
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
+
+
     @Override
     public List<String> getSubBreeds(String breed) {
-        // TODO Task 1: Complete this method based on its provided documentation
-        //      and the documentation for the dog.ceo API. You may find it helpful
-        //      to refer to the examples of using OkHttpClient from the last lab,
-        //      as well as the code for parsing JSON responses.
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+        DogApiBreedFetcher fetcher = new DogApiBreedFetcher();
+        try {
+            Request request = new Request.Builder()
+                    .url("https://dog.ceo/api/breed/" + breed + "/list")
+                    .build();
+            String subBreeds = client.newCall(request).execute().body().string();
+            JSONObject subBreedsJSON = new JSONObject(subBreeds);
+            JSONArray subBreedsJSONArray = subBreedsJSON.getJSONArray("message");
+            String subBreedsString = subBreedsJSONArray.toString();
+            String subBreedsSubString = subBreedsString.substring(1, subBreedsString.length() - 1);
+            String[] subBreedsArray = subBreedsSubString.split(",");
+            List<String> subBreedsArrayList = new ArrayList<String>();
+            for (int i = 0; i < subBreedsArray.length; i++) {
+                String subBreedsArraySubstring = subBreedsArray[i].substring(1, subBreedsArray[i].length() - 1);
+                subBreedsArrayList.add(subBreedsArraySubstring);
+            }
+            return subBreedsArrayList;
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 }
